ea58a854a6e2c9a639e2a1506f79af853c5d824c
Shahrukh Ijaz <41616030+shahrukh-ijaz@users.noreply.github.com>
2020-01-01 20:41:59 +0500

    update db

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("localhost","root","","archetypes");
+$conn=mysqli_connect("hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	","k5bdn22kogq952xm","nw6czgi7oif1hy9p","thqx6p8uwi2j8p06");
 // Check connection
 if (mysqli_connect_errno())
 {
